65b8172b63cbc0d80b7e911d8a9e1dca8bd33bd2
awaesome <awe3006@gmail.com>
2019-05-01 21:59:43 +0300

    added movie/:id route

--- routes/movies.js
+++ routes/movies.js
@@ -5,8 +5,17 @@ module.exports = server => {
   server.get('/movies', async (req, res, next) => {
     try {
       const movies = await Movie.find({}).limit(10)
-      console.log(movies)
       res.send(movies)
+      next()
+    } catch (e) {
+      return next(new errors.InvalidContentError(e))
+    }
+  })
+
+  server.get('/movies/:id', async (req, res, next) => {
+    try {
+      const movie = await Movie.findById(req.params.id)
+      res.send(movie)
       next()
     } catch (e) {
       return next(new errors.InvalidContentError(e))
